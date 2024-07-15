9183980e3d2c451169fc594b9fb777df95b8157d
jsmoriss <jsm@surniaulula.com>
2024-07-15 05:24:12 +0000

    v2.0.1

--- lib/filters-options.php
+++ lib/filters-options.php
@@ -121,6 +121,9 @@ if ( ! class_exists( 'WpssoMrpFiltersOptions' ) ) {
 			return $md_opts;
 		}
 
+		/*
+		 * Return the sanitation type for a given option key.
+		 */
 		public function filter_option_type( $type, $base_key ) {
 
 			if ( ! empty( $type ) ) {	// Return early if we already have a type.
